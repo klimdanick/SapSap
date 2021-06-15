21cb967138068eb69051d51a226c3b74daf9ee45
klimdanick <41425467+klimdanick@users.noreply.github.com>
2021-06-15 16:52:50 +0200

    real time generation

--- SapSap/Assets/terrain scripts/hexagonGenerator.cs
+++ SapSap/Assets/terrain scripts/hexagonGenerator.cs
@@ -9,40 +9,48 @@ public class hexagonGenerator : MonoBehaviour
     public float offset, offsetX;
     public float gThreshold, pThreshold, mThreshold, sThreshold;
     public float gHeight, mHeight, wHeight, sHeight;
+    public float iOff, jOff;
     //gr
     // Start is called before the first frame update
     void Start()
     {
-
-        GameObject[] prefabs = new GameObject[5];
-        prefabs[0] = mountainPrefab;
-        prefabs[1] = pondPrefab;
-        prefabs[2] = grassPrefab;
-        prefabs[3] = waterPrefab;
-        prefabs[4] = sandPrefab;
-        for (float i = 0f; i < width; i++) for (float j = 0f; j < height; j++) {
-            /*
-            int random = (int)Random.Range(0f, 3f);
-            random = random > 2 ? 2 : random < 0 ? 0 : random;
-            */
-            float noise = Mathf.PerlinNoise(i/scale + seed, j/scale + seed);
-            Debug.Log(noise);
-            GameObject tile;
-            if (noise > mThreshold) tile = prefabs[0];
-            else if (noise > gThreshold && Random.Range(0f, 1f) > 0.5f) tile = prefabs[1];
-            else if (noise > gThreshold) tile = prefabs[2];
-            else if (noise > sThreshold) tile = prefabs[4];
-            else tile = prefabs[3];
-            float x = i * offset + offsetX * j%2;
-            float y = j * offset;
-            float z = noise > mThreshold ? mHeight : noise > gThreshold ? gHeight : noise > sThreshold ? sHeight : wHeight;
-            Instantiate(tile, new Vector3(x, z, y), Quaternion.identity);
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        jOff+=0.1f;
+        removeChildObjects();
+        generate();
+    }
+    
+    void removeChildObjects() {
+
+            Transform transform;
+            for(int i = 0;i < gameObject.transform.childCount; i++)
+            {
+            transform = gameObject.transform.GetChild(i);
+            GameObject.Destroy(transform.gameObject);//The transform component is removed here, and an error will be reported when running.
+            }
+
+    }
+
+    void generate() {
         
+        for (float i = 0f; i < width; i++) for (float j = 0f; j < height; j++) {
+            float noise = Mathf.PerlinNoise((i+(int)iOff)/scale + seed, (j+(int)jOff)/scale + seed);
+            GameObject tile;
+            if (noise > mThreshold) tile = mountainPrefab;
+            else if (noise > gThreshold && false/*&& Random.Range(0f, 1f) > 0.5f*/) tile = pondPrefab;
+            else if (noise > gThreshold) tile = grassPrefab;
+            else if (noise > sThreshold) tile = sandPrefab;
+            else tile = waterPrefab;
+
+            float x = i * offset + offsetX * j%2;
+            float y = j * offset;
+            float z = noise > mThreshold ? mHeight : noise > gThreshold ? gHeight : noise > sThreshold ? sHeight : wHeight;
+            GameObject tileObject = Instantiate(tile, new Vector3(x, z, y), Quaternion.identity) as GameObject;
+            tileObject.transform.parent = gameObject.transform;
+        }
     }
 }
